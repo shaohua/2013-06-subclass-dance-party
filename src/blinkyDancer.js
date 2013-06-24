5c6c80219cb6c03e8a6d24ea67d553efc5ef0e70
Hack Reactor Students <github@hackreactor.com>
2013-06-24 16:28:01 -0700

    Remove unprofessional code

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -4,21 +4,22 @@ var BlinkyDancer = function(top, left, timeBetweenSteps){
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
+  // this.oldStep = Dancer.prototype.step;
   Dancer.apply(this, arguments);
   // console.log('this', this);
-  // this.oldStep = Dancer.prototype.step;
 };
 
 // BlinkyDancer.prototype = inherit(Dancer.prototype);
 BlinkyDancer.prototype = new Dancer();
 BlinkyDancer.prototype.constructor = BlinkyDancer;
 
-BlinkyDancer.prototype.oldStep = Dancer.prototype.step;
+// BlinkyDancer.prototype.oldStep = Dancer.prototype.step;
 
 BlinkyDancer.prototype.step = function(){
   // call the old version of step at the beginning of any call to this new version of step
   // debugger;
   // console.log("this", this);
+  this.oldStep = Dancer.prototype.step;
   this.oldStep();
 
   /* toggle() is a jQuery method to show/hide the <span> tag.
