5ce6878ecb20a9d29bacdfabbfd93af15c1ff8e3
Hack Reactor Students <github@hackreactor.com>
2013-06-24 17:20:00 -0700

    BlueDancer is working

--- src/otherDancers.js
+++ src/otherDancers.js
@@ -12,3 +12,18 @@ WhiteDancer.prototype.step = function(){
   this.oldStep();
   this.$node.toggle();
 };
+
+var BlueDancer = function(top, left, timeBetweenSteps){
+  var typeOfDancer = 'blue_dancer';
+  Dancer.call(this, top, left, timeBetweenSteps, typeOfDancer);
+  console.log('top', top, 'left', left);
+};
+
+BlueDancer.prototype = new Dancer();
+BlueDancer.prototype.constructor = BlueDancer;
+
+BlueDancer.prototype.step = function(){
+  this.oldStep = Dancer.prototype.step;
+  this.oldStep();
+  this.$node.toggle();
+};
